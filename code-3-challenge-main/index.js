59f6166da5bba4ea62e7f3113bf7334b99ca6658
ianotieno <ianotieno23@gmail.com>
2023-07-14 17:29:35 +0300

    update

--- code-3-challenge-main/index.js
+++ code-3-challenge-main/index.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const filmDataEndpoint = 'http://localhost:3000/films';
+  let filmState = {};
 
   fetch(filmDataEndpoint)
     .then(response => response.json())
     .then(films => {
-      const filmsList = document.getElementById('films');
+    const filmsList = document.getElementById('films');
 
       // Add a title to the movie list
       const listTitle = document.createElement('h2');
@@ -12,6 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
       filmsList.appendChild(listTitle);
 
       films.forEach(film => {
+        // Initialize tickets_sold for each film in filmState if not done before
+        if(!filmState[film.id]) {
+          filmState[film.id] = film.tickets_sold;
+        }
+
         const filmItem = document.createElement('li');
         filmItem.classList.add('film', 'item');
         filmItem.textContent = film.title;
@@ -42,21 +48,33 @@ document.addEventListener('DOMContentLoaded', () => {
     movieTitle.textContent = film.title;
     movieRuntime.textContent = `Runtime: ${film.runtime} minutes`;
     movieShowtime.textContent = `Showtime: ${film.showtime}`;
-    movieDescription.textContent = `Movie Description: ${film.description}`; 
-    const availableTickets = film.capacity - film.tickets_sold; // Calculate available tickets
-    availableTicketsCount.textContent = availableTickets;
+    movieDescription.textContent = ` Movie Description: ${film.description}`;
+    let availableTickets = film.capacity - filmState[film.id];
+    availableTicketsCount.textContent = `Available Tickets: ${availableTickets}`;
 
-    buyTicketBtn.onclick = () => {
+    // Remove previous click event listener if it exists
+    buyTicketBtn.replaceWith(buyTicketBtn.cloneNode(true));
+
+    // Get the fresh copy of button without any attached event
+    const freshBuyTicketBtn = document.getElementById('buy-ticket-btn');
+
+    freshBuyTicketBtn.onclick = () => {
       if (availableTickets > 0) {
-        film.tickets_sold++;
-        availableTicketsCount.textContent = film.capacity - film.tickets_sold;
-        if (film.capacity - film.tickets_sold === 0) {
-          buyTicketBtn.disabled = true;
-          buyTicketBtn.textContent = 'Sold Out';
+        filmState[film.id]++;
+        availableTickets = film.capacity - filmState[film.id];
+        availableTicketsCount.textContent = `Available Tickets: ${availableTickets}`;
+        if (availableTickets === 0) {
+          freshBuyTicketBtn.disabled = true;
+          freshBuyTicketBtn.textContent = 'Sold Out';
+        } else {
+          freshBuyTicketBtn.disabled = false;
+          freshBuyTicketBtn.textContent = 'Buy Ticket';
         }
       }
     };
   };
 });
+
+
 
 
